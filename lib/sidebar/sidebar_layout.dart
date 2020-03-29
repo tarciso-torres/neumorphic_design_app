a169da89d3dd5386f28965122ddae614e3be0aeb
Tarciso Torres <tarcisoftorres@gmail.com>
2020-03-28 23:42:16 -0300

    Inititate the create draw curve menu

--- lib/sidebar/sidebar_layout.dart
+++ lib/sidebar/sidebar_layout.dart
@@ -1,30 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:neumorphic_design_app/sidebar/sidebar_item.dart';
 
-class SidebarLayout extends StatelessWidget {
+class SidebarLayout extends StatefulWidget{
+  @override
+  _SidebarLayoutState createState() => _SidebarLayoutState();
+}
+
+class _SidebarLayoutState extends State<SidebarLayout> {
+
+  int selectedIndex = 0;
+
+  void onTabTap(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         Positioned(
-          width: 60,
+          width: 90,
           top: 0,
           bottom: 0,
           right: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Color(0xFFFF559F),
-                    Color(0xFFCF5CCF),
-                    Color(0xFFFF57AC),
-                    Color(0xFFFF6D91),
-                    Color(0xFFFF8D7E),
-                    Color(0xFFB6BAA6),
-                  ],
-                  stops: [0.05, 0.3, 0.5, 0.55, 0.8, 1],
+            child: ClipPath(
+              clipper: SidebarClipper(),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color(0xFFFF559F),
+                      Color(0xFFCF5CCF),
+                      Color(0xFFFF57AC),
+                      Color(0xFFFF6D91),
+                      Color(0xFFFF8D7E),
+                      Color(0xFFB6BAA6),
+                    ],
+                    stops: [0.05, 0.3, 0.5, 0.55, 0.8, 1],
+                  ),
                 ),
               ),
             ),
@@ -36,7 +53,7 @@ class SidebarLayout extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 SizedBox(
-                  height: 50,
+                  height: 40,
                 ),
                 Icon(Icons.dashboard, color: Colors.white),
                 SizedBox(
@@ -53,29 +70,37 @@ class SidebarLayout extends StatelessWidget {
                     children: <Widget>[
                       SidebarItem(
                         text: "Wallet",
-                        onTabTap: () {},
-                        isSelected: true,
+                        onTabTap: () {
+                          onTabTap(0);
+                        },
+                        isSelected: selectedIndex == 0,
                       ),
                       SidebarItem(
                         text: "Restaurants",
-                        onTabTap: () {},
-                        isSelected: false,
+                        onTabTap: () {
+                          onTabTap(1);
+                        },
+                        isSelected: selectedIndex == 1,
                       ),
                       SidebarItem(
                         text: "My Cart",
-                        onTabTap: () {},
-                        isSelected: false,
+                        onTabTap: () {
+                          onTabTap(2);
+                        },
+                        isSelected: selectedIndex == 2,
                       ),
                       SidebarItem(
                         text: "My Profile",
-                        onTabTap: () {},
-                        isSelected: false,
+                        onTabTap: () {
+                          onTabTap(3);
+                        },
+                        isSelected: selectedIndex == 3,
                       ),
                     ],
                   ),
                 ),
                 SizedBox(
-                  height: 50,
+                  height: 90,
                 ),
               ],
             ),
@@ -83,4 +108,33 @@ class SidebarLayout extends StatelessWidget {
         ]
       );
   }
+}
+
+class SidebarClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    double width = size.width;
+    double height = size.height;
+
+    // up curve
+    path.moveTo(width, 0);
+    path.quadraticBezierTo(width / 3, 5, width / 3, 70);
+
+    // custom curve
+
+
+    //down curve
+    path.lineTo(width / 3, height - 70);
+    path.quadraticBezierTo(width / 3, height - 5, width, height);
+
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+  
 }
