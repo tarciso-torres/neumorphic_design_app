029702fd97c77448269f39533178e52f08a805eb
Tarciso Torres <tarcisoftorres@gmail.com>
2020-03-29 22:54:57 -0300

    Create card Total Balance

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:clay_containers/clay_containers.dart';
 import 'package:flutter/material.dart';
 import 'package:neumorphic_design_app/sidebar/sidebar_layout.dart';
+
+import 'chart.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,41 +10,165 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Neumorphic Design',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage()
-    );
+        title: 'Neumorphic Design',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            scaffoldBackgroundColor: Colors.white.withOpacity(0.9)),
+        home: HomePage());
   }
 }
+
+final data = [55.0, 90.0, 50.0, 40.0, 35.0, 55.0, 70.0, 100.0];
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                height: 60,
-              ),
-              Text(
-                "Hello!",
-                style: TextStyle(fontSize: 24, color: Colors.black),
-              ),
-              Text(
-                "Tarciso Torres",
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w800
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 60,
                 ),
-              )
-            ],
+                Text(
+                  "Hello!",
+                  style: TextStyle(fontSize: 24, color: Colors.black),
+                ),
+                Text(
+                  "Tarciso Torres",
+                  style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w800),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                ClayContainer(
+                  height: 300,
+                  width: width * 0.8,
+                  depth: 12,
+                  spread: 12,
+                  borderRadius: 16,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 16.0, right: 16.0, top: 16.0),
+                        child: Text("Total Balance",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Text("R\$ 425,04",
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w900,
+                                color: Colors.black)),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Chart(data: data),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ClayContainer(
+                      height: 180,
+                      width: width * 0.35,
+                      emboss: true,
+                      borderRadius: 16,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 16.0, right: 16.0, top: 16.0),
+                            child: Text("Balance for today",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w900,
+                                    color: Colors.black45)),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text("R\$ 19,00",
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w900,
+                                    color: Colors.black45)),
+                          ),
+                          Spacer(),
+                          Container(
+                            margin: const EdgeInsets.only(
+                                bottom: 16, left: 16, right: 16),
+                            height: 10,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.black45),
+                          )
+                        ],
+                      ),
+                    ),
+                    ClayContainer(
+                      height: 180,
+                      width: width * 0.35,
+                      emboss: true,
+                      borderRadius: 16,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 16.0, right: 16.0, top: 16.0),
+                            child: Text("Balance for today",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w900,
+                                    color: Colors.black45)),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text("R\$ 19,00",
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w900,
+                                    color: Colors.black45)),
+                          ),
+                          Spacer(),
+                          Container(
+                            margin: const EdgeInsets.only(
+                                bottom: 16, left: 16, right: 16),
+                            height: 10,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.black45),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
