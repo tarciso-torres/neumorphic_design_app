207c888bd580435022a658261ff77d999747f974
Tarciso Torres <tarcisoftorres@gmail.com>
2020-04-04 17:41:30 -0300

    Implement the last cards

--- lib/main.dart
+++ lib/main.dart
@@ -50,37 +50,39 @@ class HomePage extends StatelessWidget {
                 SizedBox(
                   height: 40,
                 ),
-                ClayContainer(
-                  height: 300,
-                  width: width * 0.8,
-                  depth: 12,
-                  spread: 12,
-                  borderRadius: 16,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 16.0, right: 16.0, top: 16.0),
-                        child: Text("Total Balance",
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black)),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: Text("R\$ 425,04",
-                            style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w900,
-                                color: Colors.black)),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Chart(data: data),
-                    ],
+                Center(
+                  child: ClayContainer(
+                    height: 300,
+                    width: width * 0.8,
+                    depth: 12,
+                    spread: 12,
+                    borderRadius: 16,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 16.0, right: 16.0, top: 16.0),
+                          child: Text("Total Balance",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Text("R\$ 425,04",
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w900,
+                                  color: Colors.black)),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Chart(data: data),
+                      ],
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -130,7 +132,6 @@ class HomePage extends StatelessWidget {
                     ClayContainer(
                       height: 180,
                       width: width * 0.35,
-                      emboss: true,
                       borderRadius: 16,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -138,33 +139,219 @@ class HomePage extends StatelessWidget {
                           Padding(
                             padding: const EdgeInsets.only(
                                 left: 16.0, right: 16.0, top: 16.0),
-                            child: Text("Balance for today",
+                            child: Text("Unlock the limit of R\$ 19",
                                 style: TextStyle(
                                     fontSize: 16,
                                     fontWeight: FontWeight.w900,
-                                    color: Colors.black45)),
+                                    color: Colors.black)),
                           ),
                           Padding(
                             padding:
                                 const EdgeInsets.symmetric(horizontal: 16.0),
-                            child: Text("R\$ 19,00",
+                            child: Text("By linking your bank card",
                                 style: TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.w900,
-                                    color: Colors.black45)),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black45
+                                    )
+                                  ),
                           ),
                           Spacer(),
-                          Container(
-                            margin: const EdgeInsets.only(
-                                bottom: 16, left: 16, right: 16),
-                            height: 10,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.black45),
-                          )
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 16, right: 16),
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Color(0xFFFF559F),
+                                      Color(0xFFCF5CCF),
+                                      Color(0xFFFF57AC),
+                                      Color(0xFFFF6D91),
+                                      Color(0xFFFF8D7E),
+                                      Color(0xFFB6BAA6),
+                                    ]
+                                  )
+                              ),
+                              child: Icon(Icons.arrow_forward, color: Colors.white,),
+                            ),
+                          ),
                         ],
                       ),
                     ),
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "March 2020",
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontWeight: FontWeight.w900
+                        ),
+                      ),
+                      Text(
+                        "\$ -52.30",
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontWeight: FontWeight.w900
+                        )
+                      )
+                    ],
+                  ),
+                ),
+                Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: ClayContainer(
+                        width: 40,
+                        height: 40,
+                        borderRadius: 8,
+                        child: ShaderMask(
+                          shaderCallback: (Rect bounds) {
+                            return LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Color(0xFFFF559F),
+                                Color(0xFFCF5CCF),
+                                Color(0xFFFF57AC),
+                                Color(0xFFFF6D91),
+                                Color(0xFFFF8D7E),
+                                Color(0xFFB6BAA6),
+                              ]
+                            ).createShader(bounds);
+                          },
+                          blendMode: BlendMode.srcATop,
+                          child: Icon(
+                            Icons.fastfood,
+                            color: Colors.red,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                      title: Text(
+                        "Mc Donald's",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900
+                        ),
+                      ),
+                      subtitle: Text(
+                        "12 March 13:43",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300
+                        ),
+                      ),
+                      trailing: Text(
+                        "\$ -9.20",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: ClayContainer(
+                        width: 40,
+                        height: 40,
+                        borderRadius: 8,
+                        child: ShaderMask(
+                          shaderCallback: (Rect bounds) {
+                            return LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Color(0xFFFF559F),
+                                Color(0xFFCF5CCF),
+                                Color(0xFFFF57AC),
+                                Color(0xFFFF6D91),
+                                Color(0xFFFF8D7E),
+                                Color(0xFFB6BAA6),
+                              ]
+                            ).createShader(bounds);
+                          },
+                          blendMode: BlendMode.srcATop,
+                          child: Icon(
+                            Icons.local_pizza,
+                            color: Colors.red,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                      title: Text(
+                        "Sc Boul Andre",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900
+                        ),
+                      ),
+                      subtitle: Text(
+                        "12 March 13:43",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300
+                        ),
+                      ),
+                      trailing: Text(
+                        "\$ -9.20",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: ClayContainer(
+                        width: 40,
+                        height: 40,
+                        borderRadius: 8,
+                        child: ShaderMask(
+                          shaderCallback: (Rect bounds) {
+                            return LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Color(0xFFFF559F),
+                                Color(0xFFCF5CCF),
+                                Color(0xFFFF57AC),
+                                Color(0xFFFF6D91),
+                                Color(0xFFFF8D7E),
+                                Color(0xFFB6BAA6),
+                              ]
+                            ).createShader(bounds);
+                          },
+                          blendMode: BlendMode.srcATop,
+                          child: Icon(
+                            Icons.local_pizza,
+                            color: Colors.red,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                      title: Text(
+                        "Sc Boul Andre",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900
+                        ),
+                      ),
+                      subtitle: Text(
+                        "12 March 13:43",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300
+                        ),
+                      ),
+                      trailing: Text(
+                        "\$ -9.20",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900
+                        ),
+                      ),
+                    )
                   ],
                 )
               ],
